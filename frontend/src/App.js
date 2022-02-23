e97a52cdf81de41f5070d336fe712ab3d21e0b23
Amlan Deep Nath <amlannath95@gmail.com>
2022-02-23 13:23:56 +0530

    Frontend folder addition

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Dashboard from './Dashboard';
+import Form from './Form';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App" id="s1">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
