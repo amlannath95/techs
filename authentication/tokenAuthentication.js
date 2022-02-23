ea380354ec5ae9406fc3bd0db2142c271b73a769
Amlan Deep Nath <amlannath95@gmail.com>
2022-02-23 19:02:43 +0530

    Authentication file added

---
+++ authentication/tokenAuthentication.js
@@ -0,0 +1,35 @@
+const { type } = require("express/lib/response");
+const jwt = require("jsonwebtoken");
+
+// Checking for correct token
+module.exports = {
+    checkToken: (req, res, next) => {
+        // let token = req.get("authorization");
+        let token = req.params.token;
+        console.log(token);
+        // token = token.substr(7);
+        console.log(token);
+        if(token){
+            console.log('here');
+            // token = token.slice(7);
+            jwt.verify(token, 'secretkey123', (err, decoded) => {
+                if(err){
+                    // console.log(err);
+                    res.send({
+                        "status": false,
+                        "message": "Invalid token"
+                    });
+                }
+                else{
+                    next();
+                }
+            })
+        }
+        else{
+            res.send({
+                "status": false,
+                "message": "Access denied"
+            });
+        }
+    }
+}
